96ad13d2daa72960fe256387f0482e2b887dfa1b
cubnoble <cub.noble@yahoo.fr>
2022-03-12 14:00:05 -0600

    Player movement, Nav mesh

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject playerModel;
+
+    public float speed = .1f;
+
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(-xDirection, 0.0f, -zDirection);
+
+        transform.position += moveDirection * speed;
+
+        playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, Quaternion.LookRotation(new Vector3(xDirection, 0.0f, zDirection)), Time.deltaTime * 100);
+    }
+}
